fb4d3f023a1192dd2a113a3198951cc234e90bcf
nanangcreacoll <nanang.creacol.26@gmail.com>
2025-03-18 23:24:16 +0700

    update window size

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -2,6 +2,10 @@ return {
   {
     "nvim-neo-tree/neo-tree.nvim",
     opts = {
+      window = {
+        width = 30,
+        position = "right",
+      },
       filesystem = {
         filtered_items = {
           hide_dotfiles = false,
